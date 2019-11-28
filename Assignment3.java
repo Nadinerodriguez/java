8198b4f23a934311fcdc40fd7f309cc8bbbece19
Nadinerodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2019-11-28 00:06:52 -0500

    Update Assignment3.java

--- Assignment3.java
+++ Assignment3.java
@@ -1,1 +1,14 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
+public class Assignment3 {
+    public static void main(String[] args) {
+        Theater []days = new Theater[3]; // Simulates 3 days;
+        for (int i = 0; i < days.length; i++) {
+            days[i] = new Theater();
+            days[i].schedule();
+            days[i].sellTickets();
+        }
+    }
