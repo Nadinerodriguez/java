c73ef3bf4278613bf9f799bd3ccca5aca30fab54
Nadine Rodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2019-12-10 12:36:00 -0500

    Update Assignment5.java

--- Assignment5.java
+++ Assignment5.java
@@ -1,1 +1,21 @@
 
+class Rational {
+    private int numerator, denominator;
+
+    public Rational() {
+        numerator = 0;
+        denominator = 1;
+    }
+    public Rational(int num, int den) {
+        if (den == 0) {
+            throw new IllegalDenominatorException("Denominator cannot be zero");
+        }
+        // If denominator is a negative number, this will make the numerator a negative number instead
+        if (den < 0) {
+            den = -den;
+            num = -num;
+        }
+        numerator = num;
+        denominator = den;
+    }
+}
