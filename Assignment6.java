3bb0018745dc258eb9eddcec1a7b306c499ecbf7
Nadine Rodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2020-01-22 08:44:33 -0500

    Update Assignment6.java

--- Assignment6.java
+++ Assignment6.java
@@ -1,1 +1,30 @@
+class Node<T> {
+    private T data;
+    private Node<T> next;
 
+    public Node() {
+        data = null;
+        next = null;
+    }
+
+    public Node(T d, Node<T> n) {
+        data = d;
+        next = n;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setData(T d) {
+        data = d;
+    }
+
+    public void setNext(Node<T> n) {
+        next = n;
+    }
+}
